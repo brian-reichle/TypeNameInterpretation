a7dded92f8cdfedd627f9f50d2f23a22bc4a5a43
brian-reichle <18721383+brian-reichle@users.noreply.github.com>
2022-02-06 13:28:40 +1000

    Provide delimiters as a span.

--- src/TypeNameInterpretation/Delimiters.cs
+++ src/TypeNameInterpretation/Delimiters.cs
@@ -1,9 +1,13 @@
 // Copyright (c) Brian Reichle.  All Rights Reserved.  Licensed under the MIT License.  See LICENSE in the project root for license information.
+using System;
+
 namespace TypeNameInterpretation
 {
 	static class Delimiters
 	{
-		public static readonly char[] All = { '\\', '[', ']', ',', '+', '&', '*', '=', '"' };
-		public static readonly char[] Quote = { '\\', '"' };
+		public static ReadOnlySpan<char> All => _allDelimiterChars;
+		public static ReadOnlySpan<char> Quote => _allDelimiterChars.AsSpan(0, 2);
+
+		static readonly char[] _allDelimiterChars = { '\\', '"', '[', ']', ',', '+', '&', '*', '=' };
 	}
 }
