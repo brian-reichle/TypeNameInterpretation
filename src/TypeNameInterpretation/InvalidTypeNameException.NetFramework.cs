5bcc16a8f7444e42f3b82d2c6726da87a653040f
brian-reichle <18721383+brian-reichle@users.noreply.github.com>
2024-03-03 22:20:41 +1000

    Exceptions should only be serialisable when targeting netframework or netstandard.

---
+++ src/TypeNameInterpretation/InvalidTypeNameException.NetFramework.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Brian Reichle.  All Rights Reserved.  Licensed under the MIT License.  See LICENSE in the project root for license information.
+#if NETFRAMEWORK || NETSTANDARD
+using System;
+using System.Runtime.Serialization;
+
+namespace TypeNameInterpretation
+{
+	[Serializable]
+	partial class InvalidTypeNameException
+	{
+		InvalidTypeNameException(SerializationInfo info, StreamingContext context)
+			: base(info, context)
+		{
+		}
+	}
+}
+#endif
