e836a2be3ddbe9da776798482994ae618723c3ee
brian-reichle <18721383+brian-reichle@users.noreply.github.com>
2026-02-13 20:35:59 +1000

    Added shim for ArgumentNullException.ThrowIfNull.

---
+++ src/TypeNameInterpretation/FrameworkExtensions/ArgumentNullExceptionShims.cs
@@ -0,0 +1,26 @@
+// Copyright (c) Brian Reichle.  All Rights Reserved.  Licensed under the MIT License.  See LICENSE in the project root for license information.
+
+#if !NET
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace System
+{
+	static class ArgumentNullExceptionShims
+	{
+		extension(ArgumentNullException)
+		{
+			public static void ThrowIfNull([NotNull] object? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+			{
+				if (argument is null)
+				{
+					ThrowNullReferenceException(paramName);
+				}
+			}
+		}
+
+		[DoesNotReturn]
+		static void ThrowNullReferenceException(string? paramName) => throw new ArgumentNullException(paramName);
+	}
+}
+#endif
---
+++ src/TypeNameInterpretation/FrameworkExtensions/Attributes/CallerArgumentExpressionAttribute.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Brian Reichle.  All Rights Reserved.  Licensed under the MIT License.  See LICENSE in the project root for license information.
+
+#if !NET
+namespace System.Runtime.CompilerServices
+{
+	[AttributeUsage(AttributeTargets.Parameter)]
+	sealed class CallerArgumentExpressionAttribute(string parameterName) : Attribute
+	{
+		public string ParameterName { get; } = parameterName;
+	}
+}
+#endif
