39b1c8abd5923e6eaeb1ce244419421abd59cfc9
brian-reichle <18721383+brian-reichle@users.noreply.github.com>
2026-02-20 20:38:53 +1000

    Updated target frameworks for benchmarking.

--- src/TypeNameInterpretation.Benchmark/FormatAssemblyName.cs
+++ src/TypeNameInterpretation.Benchmark/FormatAssemblyName.cs
@@ -1,9 +1,13 @@
 // Copyright (c) Brian Reichle.  All Rights Reserved.  Licensed under the MIT License.  See LICENSE in the project root for license information.
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
 using static TypeNameInterpretation.InsTypeFactory;
 
 namespace TypeNameInterpretation.Benchmark;
 
+[SimpleJob(RuntimeMoniker.Net48)]
+[SimpleJob(RuntimeMoniker.Net80)]
+[SimpleJob(RuntimeMoniker.Net10_0)]
 [MemoryDiagnoser]
 public class FormatAssemblyName
 {
